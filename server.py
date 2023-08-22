834337dc40c2f4464ff3e071c536f66ad4480231
Vinayaka0204 <vinayakahegde94@gmail.com>
2023-08-22 19:42:10 +0530

    Defined the functions

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!', 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    jsonrequest = requests.json()
+    first = jsonrequest['first']
+    second = jsonrequest['second']
+    res = first+second
+    return res, 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    jsonrequest = requests.json()
+    first = jsonrequest['first']
+    second = jsonrequest['second']
+    res = first-second
+    return res, 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
